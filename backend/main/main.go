1a97248246bd7a1ef05e48fb1e20f5550a5cdfbc
Dragan Mitrašinović <dragan.mitrasinovic19@gmail.com>
2023-07-18 11:04:09 +0200

    Made a controller with CRUD operations

--- backend/main/main.go
+++ backend/main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"backend/config"
+	"backend/controller"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Gm")
+	e := echo.New()
+
+	lotteryRoute := e.Group("/lottery")
+	lotteryRoute.POST("", controller.CreateLottery)
+	lotteryRoute.PUT("", controller.DepositToLottery)
+	lotteryRoute.GET("", controller.GetAllActiveLotteries)
+	lotteryRoute.GET("/:address", controller.GetAllLotteriesForUser)
+	lotteryRoute.DELETE("/:address/:id", controller.WithdrawFromLottery)
+
+	config.DatabaseInit()
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
