f2c4757fe017c377cc5c9dc0399aa6ff046c8dee
Dragan Mitrašinović <dragan.mitrasinovic19@gmail.com>
2023-07-19 14:59:10 +0200

    Added .env

--- backend/config/db.go
+++ backend/config/db.go
@@ -3,19 +3,25 @@ package config
 import (
 	"backend/model"
 	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var database *gorm.DB
-var err error
 
 func DatabaseInit() {
-	host := "localhost"
-	user := "root"
-	password := "root"
-	dbName := "no_loss_lottery"
-	port := 3306
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbName)
 
 	database, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
