afe1e568dc4c81b917d78be6cc1fec7e08a651f1
Rajko Zagorac <rajkozgrc4@gmail.com>
2023-07-21 22:06:23 +0200

    Automate script for creating test lotteries

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000,
+        runs: 1
       },
     },
   },
--- contracts/scripts/deployLotteryFactory.js
+++ contracts/scripts/deployLotteryFactory.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
+const fs = require('fs');
+const {resolve, join} = require("path");
 
 async function main() {
-    await hre.run("compile");
-
     const LotteryFactory = await hre.ethers.getContractFactory("LotteryFactory");
     const lotteryFactory = await LotteryFactory.deploy();
     await lotteryFactory.waitForDeployment();
@@ -10,6 +10,46 @@ async function main() {
     const addressContract = await lotteryFactory.getAddress();
     console.log("LotteryFactory address:" + addressContract);
 }
+
+async function deployLotteryFactory() {
+    const LotteryFactory = await hre.ethers.getContractFactory("LotteryFactory");
+    const lotteryFactory = await LotteryFactory.deploy();
+    await lotteryFactory.waitForDeployment();
+
+    const addressContract = await lotteryFactory.getAddress();
+    console.log("LotteryFactory address:" + addressContract);
+
+    saveNewAddressToFile(addressContract);
+
+    return lotteryFactory;
+}
+
+function saveNewAddressToFile(address) {
+    const content =
+`import LotteryFactory from "./build/ILotteryFactory.json";
+import Web3 from "web3";
+
+const web3 = new Web3(window.ethereum);
+
+export const lotteryFactoryContractAddress =  '${address}'
+
+export const LotteryFactoryContract = new web3.eth.Contract(
+    LotteryFactory.abi,
+    lotteryFactoryContractAddress
+);`;
+
+    const dirPath = resolve(__dirname, '../../frontend/src/ethereum');
+    const filePath = join(dirPath, 'LotteryFactoryContract.js');
+
+    try {
+        fs.writeFileSync(filePath, content);
+        console.log("File written successfully");
+    } catch (error) {
+        console.error(`Error writing file: ${error}`);
+    }
+}
+
+module.exports = { deployLotteryFactory };
 
 main().catch((error) => {
     console.error(error);
