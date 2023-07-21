cbd17a6d0bf18f668c79ec99d35aeaa1653e70c8
Dragan Mitrašinović <dragan.mitrasinovic19@gmail.com>
2023-07-21 11:17:10 +0200

    Fix FindAllEnded bug

--- backend/db/gorm/impl.go
+++ backend/db/gorm/impl.go
@@ -108,7 +108,7 @@ func (lg LotteryGorm) CreateLottery(lottery model.Lottery) error {
 
 func (lg LotteryGorm) FindAllEnded() ([]model.Lottery, error) {
 	var lotteries []DBLottery
-	err := lg.DB.Find(&lotteries, "active = ", false).Error
+	err := lg.DB.Find(&lotteries, "active = ?", false).Error
 	if err != nil {
 		return nil, err
 	}
@@ -127,5 +127,10 @@ func (lg LotteryGorm) FindAllEnded() ([]model.Lottery, error) {
 			Winner:        lottery.Winner,
 		})
 	}
+
+	if result == nil {
+		result = []model.Lottery{}
+	}
+
 	return result, nil
 }
