5a8e4e6f9ddf5ea8af3207108cd5680efe74b43f
Brankonymous <brankogrbic01@gmail.com>
2023-07-21 10:52:04 +0200

    fixed Past lotteries and toFixed bug

--- frontend/src/components/Lottery/ArchivedLottery.jsx
+++ frontend/src/components/Lottery/ArchivedLottery.jsx
@@ -4,12 +4,7 @@ import "./Lottery.scss"
 const ArchivedLottery = ({ archivedLottery }) => {
 
     const { contractAddress, protocol, tokenSymbol, lastTVL, lastTVLUSD, yieldUSD, endDate, winner } = archivedLottery
-    const achievedYield = archivedLottery.yield.toFixed(2)
-
-    useEffect(() => {
-        console.log("ola")
-        console.log("archivedLottery", archivedLottery)
-    }, [])
+    const achievedYield = archivedLottery.yield?.toFixed(2)
 
     const protocolImage = (protocol) => {
         if (protocol === "Aave V3") {
@@ -26,25 +21,27 @@ const ArchivedLottery = ({ archivedLottery }) => {
     }
 
     return (
-        <div className="one-lottery">
-            <div className="oneLotteryName">
-                <img className='imageLogo' src={protocolImage(protocol)} alt={protocol}/> 
-                {contractAddress}
-                <img className='imageLogo' src={tokenImage(tokenSymbol)} alt={tokenSymbol}/> 
+        <button className="oneLotteryButton">
+            <div className="one-lottery">
+                <div className="oneLotteryName">
+                    <img className='imageLogo' src={protocolImage(protocol)} alt={protocol}/> 
+                    {contractAddress}
+                    <img className='imageLogo' src={tokenImage(tokenSymbol)} alt={tokenSymbol}/> 
+                </div>
+                <div className="oneLotteryLine">
+                    <div className="last-tvl">{`Last TVL: ${lastTVL} ${tokenSymbol} (${lastTVLUSD?.toFixed(2)} $)`}</div>
+                </div>
+                <div className="oneLotteryLine">
+                    <div className="yield">{`Yield: ${achievedYield} ${tokenSymbol} (${yieldUSD?.toFixed(2)} $)`}</div>
+                </div>
+                <div className="oneLotteryLine">
+                    <div className="winner">{`Winner: ${winner}`}</div>
+                </div>
+                <div className="oneLotteryLine">
+                    <div className="end-date">{`Ended on: ${convertUnixTimestampToDate()}`}</div>
+                </div>
             </div>
-            <div className="oneLotteryLine">
-                <div className="last-tvl">{`Last TVL: ${lastTVL} ${tokenSymbol} (${lastTVLUSD?.toFixed(2)} $)`}</div>
-            </div>
-            <div className="oneLotteryLine">
-                <div className="yield">{`Yield: ${achievedYield} ${tokenSymbol} (${yieldUSD?.toFixed(2)} $)`}</div>
-            </div>
-            <div className="oneLotteryLine">
-                <div className="winner">{`Winner: ${winner}`}</div>
-            </div>
-            <div className="oneLotteryLine">
-                <div className="end-date">{`Ended on: ${convertUnixTimestampToDate()}`}</div>
-            </div>
-        </div>
+        </button>
     )
 }
 
--- frontend/src/components/modals/ClaimModal.jsx
+++ frontend/src/components/modals/ClaimModal.jsx
@@ -36,10 +36,10 @@ const ClaimModal = ({ isOpen, closeModal, wallet, lottery }) => {
         <div>
             <div className='modalState'>
                 <label className='modalLabel'> <p style={{color: 'green'}}>Congratulations! You are the winner </p></label>
-                <label className='modalLabel'> You have deposited {myAmount} {tokenSymbol}, which is {`${myAmountUSD.toFixed(4)}$`}</label>
-                <label className='modalLabel'> The reward for this lottery was {reward} {tokenSymbol}, which is {`${rewardUSD.toFixed(4)}$`}</label>
+                <label className='modalLabel'> You have deposited {myAmount} {tokenSymbol}, which is {`${myAmountUSD?.toFixed(4)}$`}</label>
+                <label className='modalLabel'> The reward for this lottery was {reward} {tokenSymbol}, which is {`${rewardUSD?.toFixed(4)}$`}</label>
                 <br></br>
-                <label className='modalLabel'> <h2>You can claim {`${moneyToClaim.toFixed(4)}$`}</h2></label>
+                <label className='modalLabel'> <h2>You can claim {`${moneyToClaim?.toFixed(4)}$`}</h2></label>
             </div>
 
             <div className='flexRowDiv'>
