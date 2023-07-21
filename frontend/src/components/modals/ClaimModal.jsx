5c6dc4bf6df03c27d16e86d262fdb1d687d701c7
Brankonymous <brankogrbic01@gmail.com>
2023-07-21 18:23:21 +0200

    smart contract integration for deposit

--- frontend/src/components/modals/ClaimModal.jsx
+++ frontend/src/components/modals/ClaimModal.jsx
@@ -4,14 +4,16 @@ import { tokenToUSD } from '../../providers/OracleProvider';
 import { getLotteryReward, withdrawMoneyFromLottery } from '../../providers/LotteryProvider';
 
 const ClaimModal = ({ isOpen, closeModal, wallet, lottery }) => {
-
+    const [isLoading, setIsLoading] = useState(false)
     const [reward, setReward] = useState(0)
     const [rewardUSD, setRewardUSD] = useState(0)
     const [moneyToClaim, setMoneyToClaim] = useState(0)
     const { contractAddress, tokenSymbol, myAmount, myAmountUSD } = lottery
 
     const handleClaim = async () => {
+        setIsLoading(true)
         await withdrawMoneyFromLottery(wallet, contractAddress)
+        setIsLoading(false)
         closeModal()
     }
 
@@ -32,21 +34,26 @@ const ClaimModal = ({ isOpen, closeModal, wallet, lottery }) => {
 
     return (
         <Modal className='lotteryModal' isOpen={isOpen} onRequestClose={closeModal} contentLabel="Claim Modal">
-        <h1 className='modalName'>Claim Money</h1>
-        <div>
-            <div className='modalState'>
-                <label className='modalLabel'> <p style={{color: 'green'}}>Congratulations! You are the winner </p></label>
-                <label className='modalLabel'> You have deposited {myAmount} {tokenSymbol}, which is {`${myAmountUSD?.toFixed(4)}$`}</label>
-                <label className='modalLabel'> The reward for this lottery was {reward} {tokenSymbol}, which is {`${rewardUSD?.toFixed(4)}$`}</label>
-                <br></br>
-                <label className='modalLabel'> <h2>You can claim {`${moneyToClaim?.toFixed(4)}$`}</h2></label>
-            </div>
+        {isLoading ? 
+        <><div className='loading'>Loading...</div> <div className='loader'></div></> : (
+        <>
+            <h1 className='modalName'>Claim Money</h1>
+            <div>
+                <div className='modalState'>
+                    <label className='modalLabel'> <p style={{color: 'green'}}>Congratulations! You are the winner </p></label>
+                    <label className='modalLabel'> You have deposited {myAmount} {tokenSymbol}, which is {`${myAmountUSD?.toFixed(4)}$`}</label>
+                    <label className='modalLabel'> The reward for this lottery was {reward} {tokenSymbol}, which is {`${rewardUSD?.toFixed(4)}$`}</label>
+                    <br></br>
+                    <label className='modalLabel'> <h2>You can claim {`${moneyToClaim?.toFixed(4)}$`}</h2></label>
+                </div>
 
-            <div className='flexRowDiv'>
-                <button className='modalButton' onClick={handleClaim}>Claim</button>
-                <button className='modalButton' onClick={closeModal}>Cancel</button>
+                <div className='flexRowDiv'>
+                    <button className='modalButton' onClick={handleClaim}>Claim</button>
+                    <button className='modalButton' onClick={closeModal}>Cancel</button>
+                </div>
             </div>
-        </div>
+        </>
+        )}
         </Modal>
     )
 }
